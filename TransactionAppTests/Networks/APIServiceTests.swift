e96a568ea9a116909129c806c52e1b0680f0e7f9
Gautam kumar singh <gautam.singh@konicaminolta.com>
2021-11-13 03:08:14 +0800

    Added testcase for APIService and dashboard interactor

---
+++ TransactionAppTests/Networks/APIServiceTests.swift
@@ -0,0 +1,233 @@
+//
+//  APIServiceTests.swift
+//  TransactionAppTests
+//
+//  Created by Gautam Singh on 13/11/21.
+//
+
+import XCTest
+
+@testable import TransactionApp
+
+
+final class APIServiceTests: XCTestCase {
+    var sut: ServiceProtocol!
+    override func setUp() {
+        sut = APIService(APIManager())
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    func test_interactor_with_empty_username() {
+        //Given
+        let loginExpectation = self.expectation(description: "Call Login API")
+        let badRequest = Utils.getLocalisedValue(key:"Bad_Request")
+        let userModel = UserModel(username: "", password: "344")
+
+        //When
+        sut.startLogin(user: userModel, on: { (response, error) in
+            loginExpectation.fulfill()
+            //Then
+            XCTAssertEqual(response?.status, Utils.getLocalisedValue(key: "Failed"))
+            XCTAssertNil(response?.token)
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error!.message, badRequest)
+        })
+        
+        waitForExpectations(timeout: 1.0, handler: nil)
+        
+    }
+    func test_interactor_with_empty_passsword() {
+        //Given
+        let loginExpectation = self.expectation(description: "Call Login API")
+        let badRequest = Utils.getLocalisedValue(key:"Bad_Request")
+        let userModel = UserModel(username: "22", password: "")
+
+        //When
+        sut.startLogin(user: userModel, on: { (response, error) in
+            loginExpectation.fulfill()
+            //Then
+            XCTAssertEqual(response?.status, Utils.getLocalisedValue(key: "Failed"))
+            XCTAssertNil(response?.token)
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error!.message, badRequest)
+        })
+        
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+
+    func test_valid_credentials_value() {
+        //Given
+        let loginExpectation = self.expectation(description: "Call Login API")
+        let userModel = UserModel(username: "xxxxx", password: "yyyyy")
+
+        //When
+        sut.startLogin(user: userModel, on: { (response, error) in
+            loginExpectation.fulfill()
+            //Then
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error!.message,Utils.getLocalisedValue(key: "ForBidden"))
+        })
+        
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    func test_validate_credentails_on_server(){
+        //Given
+        let loginExpectation = self.expectation(description: "Call Login API")
+        let userModel = UserModel(username: "ocbc", password: "123456")
+
+        //When
+        sut.startLogin(user: userModel, on: { (response, error) in
+            loginExpectation.fulfill()
+            //Then
+            if response != nil {
+                XCTAssertNotNil(response)
+                XCTAssertNotNil(response?.token)
+                XCTAssertEqual(response?.status, "success")
+            }
+        })
+        
+        waitForExpectations(timeout: 1.0, handler: nil)
+    }
+    
+    func test_check_balance(){
+        //Given
+        let loginExpectation = self.expectation(description: "Call Login API")
+        let checkBalanceExp = self.expectation(description: "Check balance")
+        let userModel = UserModel(username: "ocbc", password: "123456")
+
+        //When
+        sut.startLogin(user: userModel, on: {[weak self] (response, error) in
+            loginExpectation.fulfill()
+            //Then
+            if response != nil {
+                guard let self = self  else {return}
+                if let token = response?.token{
+                    //Given
+                    TransactionManager.shared.token = token
+                
+                    //When
+                    self.sut.checkBalances { (balResponse, bError) in
+                        checkBalanceExp.fulfill()
+                        TransactionManager.shared.token = ""
+                        if balResponse != nil{
+                            XCTAssertEqual(balResponse?.status, "success")
+                        }
+                    }
+                }
+            } else {
+                XCTAssertNotNil(error)
+                XCTAssertTrue(error!.message!.count > 0)
+            }
+        })
+        
+        wait(for: [loginExpectation,checkBalanceExp], timeout: 3.0)
+    }
+    
+    func test_all_transactions(){
+        //Given
+        let loginExpectation = self.expectation(description: "Call Login API")
+        let transactionExp = self.expectation(description: "All transaction api call")
+        let userModel = UserModel(username: "ocbc", password: "123456")
+
+        //When
+        sut.startLogin(user: userModel, on: {[weak self] (response, error) in
+            loginExpectation.fulfill()
+            //Then
+            if response != nil {
+                guard let self = self  else {return}
+                if let token = response?.token{
+                    //Given
+                    TransactionManager.shared.token = token
+                    //When
+                    self.sut.getAllTransactions { (tResponse, tError) in
+                        transactionExp.fulfill()
+                        TransactionManager.shared.token = ""
+                        if tResponse != nil{
+                            XCTAssertEqual(tResponse?.status, "success")
+                        }
+                    }
+                }
+            } else {
+                XCTAssertNotNil(error)
+                XCTAssertTrue(error!.message!.count > 0)
+            }
+        })
+        
+        wait(for: [loginExpectation,transactionExp], timeout: 3.0)
+    }
+    
+    func test_all_payee(){
+        //Given
+        let loginExpectation = self.expectation(description: "Call Login API")
+        let payeeExp = self.expectation(description: "All payee api call")
+        let userModel = UserModel(username: "ocbc", password: "123456")
+
+        //When
+        sut.startLogin(user: userModel, on: {[weak self] (response, error) in
+            loginExpectation.fulfill()
+            //Then
+            if response != nil {
+                guard let self = self  else {return}
+                if let token = response?.token{
+                    //Given
+                    TransactionManager.shared.token = token
+                    //When
+                    self.sut.getAllPayee { (pResponse, pError) in
+                        payeeExp.fulfill()
+                        TransactionManager.shared.token = ""
+                        if pResponse != nil{
+                            XCTAssertEqual(pResponse?.status, "success")
+                        }
+                    }
+                }
+            } else {
+                XCTAssertNotNil(error)
+                XCTAssertTrue(error!.message!.count > 0)
+            }
+        })
+        
+        wait(for: [loginExpectation,payeeExp], timeout: 3.0)
+    }
+    func test_fund_transfer(){
+        //Given
+        let loginExpectation = self.expectation(description: "Call Login API")
+        let transferExp = self.expectation(description: "transfer api call")
+        let userModel = UserModel(username: "ocbc", password: "123456")
+
+        //When
+        sut.startLogin(user: userModel, on: {[weak self] (response, error) in
+            loginExpectation.fulfill()
+            //Then
+            if response != nil {
+                guard let self = self  else {return}
+                if let token = response?.token{
+                    //Given
+                    TransactionManager.shared.token = token
+                    //When
+                    let transferModel = TransferSceneModel(recipientAccountNo: "1234", amount: "100", date: Date().convertToString(), description: "Rental")
+                    
+                    self.sut.fundTransfer(params: transferModel.jsonValue()!) { (ftResponse, ftError) in
+                        transferExp.fulfill()
+                        TransactionManager.shared.token = ""
+                        if ftResponse != nil{
+                            XCTAssertEqual(ftResponse?.status, "success")
+                            XCTAssertEqual(ftResponse?.data?.amount, "100")
+                            XCTAssertEqual(ftResponse?.data?.description, "Rental")
+                            XCTAssertEqual(ftResponse?.data?.recipientAccountNo, "1234")
+                        }
+                    }
+                }
+            } else {
+                XCTAssertNotNil(error)
+                XCTAssertTrue(error!.message!.count > 0)
+            }
+        })
+        
+        wait(for: [loginExpectation,transferExp], timeout: 3.0)
+    }
+    
+}
+
--- TransactionAppTests/Scenes/Dashboard/DashboardSceneInteractorTests.swift
+++ TransactionAppTests/Scenes/Dashboard/DashboardSceneInteractorTests.swift
@@ -11,12 +11,121 @@ import XCTest
 
 
 final class DashboardSceneInteractorTests: XCTestCase {
+    private var sut: DashboardSceneInteractor!
+    private var presenter: DashboardScenePresenterInputMock!
+    override func setUp() {
+        super.setUp()
+        sut = DashboardSceneInteractor()
+        presenter = DashboardScenePresenterInputMock()
+        sut.presenter = presenter
+        sut.service = APIService(APIManager())
+    }
     
-  override func setUp() {
-   
-  }
-  
-  override func tearDown() {
+    override func tearDown() {
+        sut = nil
+        //service = nil
+        presenter = nil
+        super.tearDown()
+    }
     
-  }
+    func test_checkBalance_by_mock_data(){
+        //Given
+        TransactionManager.shared.enableMock = true
+        let expection = self.expectation(description: "Balance fetching expection")
+        //When
+        sut.service?.checkBalances(on: { (response, error) in
+            expection.fulfill()
+            TransactionManager.shared.enableMock = false
+            //Then
+            XCTAssertNotNil(response)
+            XCTAssertNil(error)
+            XCTAssertEqual(Float(response!.balance!), 100000.0)
+            
+        })
+        waitForExpectations(timeout: 1)
+    }
+    func test_checkBalance_from_server(){
+        //Given
+        let expection = self.expectation(description: "Balance fetching expection")
+        //When
+        //This API call always failed due to empty token
+        sut.service?.checkBalances(on: { (_, error) in
+            expection.fulfill()
+            //Then
+            XCTAssertNotNil(error)
+            XCTAssertNotNil(error?.message)
+        })
+        waitForExpectations(timeout: 1)
+    }
+    
+    func test_allTransaction_by_mock_data(){
+        //Given
+        TransactionManager.shared.enableMock = true
+        let expection = self.expectation(description: "Download all transaction expection")
+        //When
+        sut.service?.getAllTransactions(on: { (response, error) in
+            expection.fulfill()
+            TransactionManager.shared.enableMock = false
+            //Then
+            XCTAssertNotNil(response)
+            XCTAssertNil(error)
+            XCTAssertEqual(response?.data?.count, 5)
+        })
+        waitForExpectations(timeout: 1)
+    }
+    func test_allTransaction_from_server(){
+        //Given
+        let expection = self.expectation(description: "Download all transaction expection")
+        //When
+        //This API call always failed due to empty token
+        sut.service?.getAllTransactions(on: { (_, error) in
+            expection.fulfill()
+            //Then
+            XCTAssertNotNil(error)
+            XCTAssertNotNil(error?.message)
+        })
+        waitForExpectations(timeout: 1)
+    }
+    func test_balance_presenter_call(){
+        //Given
+        let bundle = Bundle(for: DashboardSceneInteractorTests.self)
+        guard let balance:BalanceResponse = Utils.load(bundle: bundle, fileName: "Balance") else {
+            XCTFail("Failed to load Balance.json")
+            return
+        }
+        //When
+        sut.presenter?.showBalance(response: balance)
+        //Then
+        XCTAssertEqual(presenter.amount, 100000.0)
+    }
+    
+    func test_show_transaction_presenter_call(){
+        //Given
+        let bundle = Bundle(for: DashboardSceneInteractorTests.self)
+        guard let tns:TransactionResponse = Utils.load(bundle: bundle, fileName: "Transactions") else {
+            XCTFail("Failed to load Balance.json")
+            return
+        }
+        //When
+        sut.presenter?.showTransactions(response: tns)
+        //Then
+        XCTAssertEqual(presenter.transCount, 5)
+    }
+    
+}
+
+private final class DashboardScenePresenterInputMock: DashboardScenePresenterInput {
+    
+    var transCount: Int = 0
+    func showTransactions(response: TransactionResponse){
+        transCount = response.data!.count
+    }
+    
+    var amount: Float = 0.0
+    func showBalance(response: BalanceResponse) {
+        amount = Float(response.balance!)
+    }
+    
+    func didFailedToLoad(error: String?) {
+    }
 }
